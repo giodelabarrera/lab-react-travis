b27225cb4b5bc4081995306472973a353366be1f
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-03-25 08:26:29 +0100

    add other test

--- cypress/integration/app_page.spec.js
+++ cypress/integration/app_page.spec.js
@@ -10,4 +10,10 @@ describe("App Page", function() {
     cy.visit("/"); // change URL to match your dev URL
     expect(true).to.equal(true);
   });
+
+  it("third successfully loads", function() {
+    // eslint-disable-next-line no-undef
+    cy.visit("/"); // change URL to match your dev URL
+    expect(true).to.equal(true);
+  });
 });
